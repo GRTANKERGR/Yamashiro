a572490b9c69ead094daeae559b3551d5cac84e6
Whatareyoulaughingat <nikosnaoumv2@outlook.com>
2020-06-14 18:42:34 +0300

    FIxed Client

--- src/YamashiroClient.py
+++ src/YamashiroClient.py
@@ -1,10 +1,13 @@
 import discord
-client = discord.Client()
+
+class YamashiroClient(discord.Client):
+
+    async def on_ready(self):
+        print("Yamashiro is ready!")
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
 
 
-@client.event
-async def on_ready():
-    print("Yamashiro is ready!")
-
-
+client = YamashiroClient()
 client.run("NzE5OTA2MzAyMDM3NTkwMTM4.XuI3Gg.1HkD1dm9tBhcDpwdPv92bjZqqho")
