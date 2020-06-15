0081e2b7147782af7013cdf58c556fdc5addc2c3
Whatareyoulaughingat <nikosnaoumv2@outlook.com>
2020-06-15 13:51:30 +0300

    Created new command: Ping

---
+++ src/cogs/ping.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+client = discord.Client()
+
+
+class Ping(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Yamashiro is online!")
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f"Pong! | Latency: {round(client.latency * 1000)}ms")
+
+def setup(client):
+    client.add_cog(Ping(client))
